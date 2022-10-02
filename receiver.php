00b74bed684d68102ed6f2dc956d485c944badfa
Alexis <alexiscasado2015@gmail.com>
2022-10-02 17:02:19 -0400

    First Commit

---
+++ receiver.php
@@ -0,0 +1,6 @@
+<?php
+echo "The form was received by: " . $_POST['recipient'] . "</br>"; 
+echo "The subject is : " . $_POST['subject']. "</br>";
+echo "The name entered is : " . $_POST['name'];
+?>
+
