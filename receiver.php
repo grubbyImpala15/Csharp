e3514a07a84736204ec014b27d62f518768e8e9e
grubbyImpala15 <114881300+grubbyImpala15@users.noreply.github.com>
2022-10-10 21:58:34 -0400

    test.html

--- receiver.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "The form was received by: " . $_POST['recipient'] . "</br>"; 
-echo "The subject is : " . $_POST['subject']. "</br>";
-echo "The name entered is : " . $_POST['name'];
-?>
-
